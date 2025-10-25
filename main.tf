2e74c0bde38080d66e258bc0f53a9c8b5d2a0e10
rakshita madival <rakshita.m.madival@gmail.com>
2025-10-25 16:27:02 +0530

    Task 4: Initial commit - Terraform Docker container

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx_container" {
+  image = docker_image.nginx.name
+  name  = "terraform-nginx"
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
